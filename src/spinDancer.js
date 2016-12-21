50446e39070e67d20c07f9fe3c431ffe785b068b
speschellk <kaychristensen@gmail.com>
2016-12-20 16:16:59 -0800

    added a spin condition to spinning sloth

--- src/spinDancer.js
+++ src/spinDancer.js
@@ -1,6 +1,6 @@
 var MakeSpinDancer = function(top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
-  this.$node.append('<img src="/Users/student/Desktop/hrsf53-subclass-dance-party/assets/sloths/raisedaroofsloth.png">');
+  this.$node.append('<img class="spin" src="/Users/student/Desktop/hrsf53-subclass-dance-party/assets/sloths/raisedaroofsloth.png">');
 };
 
 MakeSpinDancer.prototype = Object.create(MakeDancer.prototype);
@@ -8,6 +8,4 @@ MakeSpinDancer.prototype.constructor = MakeSpinDancer;
 
 MakeSpinDancer.prototype.step = function() {
   MakeDancer.prototype.step.call(this);
-  // spin action
-  this.$node.toggle();
 };
